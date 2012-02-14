df3e68586d72b41296a018c1ac5ded7a71c76d03
Thomas Sturges-Allard <zig13@termisoc.org>
2012-02-14 15:40:12 +0000

    Initial setup batch file which will set file types and file associations for the file extensions used by Agenzig

---
+++ setup.bat
@@ -0,0 +1,6 @@
+assoc.agez=Agenzig.Adventure
+assoc.azc=Agenzig.Character
+assoc.aza=Agenzig.Audio
+ASSOC.azg=Agenzig.Graphic
+FTYPE Agenzig.Audio="%~dp0<audiprogram>.exe" %1 -play
+FTYPE Agenzig.Graphic="%~dp0<graphicviewer>.exe" %1
