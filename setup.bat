523d08d7c218a36421be332f923e6deb597504ee
Thomas Sturges-Allard <zig13@termisoc.org>
2012-09-10 15:50:54 +0100

    Removed file associations as no longer needed with new graphics & audio system

--- setup.bat
+++ setup.bat
@@ -1,6 +1,4 @@
 assoc.agez=Agenzig.Adventure
 assoc.azc=Agenzig.Character
 assoc.aza=Agenzig.Audio
-ASSOC.azg=Agenzig.Graphic
-FTYPE Agenzig.Audio="%~dp0<audiprogram>.exe" %1 -play
-FTYPE Agenzig.Graphic="%~dp0<graphicviewer>.exe" %1
+assoc.azg=Agenzig.Graphic
