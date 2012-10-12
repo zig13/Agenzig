ddd0aa660c42d0301095f82eb82e2adcce03c0ad
Thomas Sturges-Allard <zig13@termisoc.org>
2012-10-12 12:29:31 +0100

    Optimisations to character creation

--- agccreator.py
+++ agccreator.py
@@ -11,10 +11,9 @@
 #-------------------------------------------------------------------------------
 def createchar( advfolder ) :
 	charname = raw_input("Please enter a name for your character >" )
-	from os import curdir
-	dot = str(curdir)
-	from os import sep
-	sep = str(sep)
+	from os import curdir, sep
+	dot = str(curdir) #The character used by the current os to denote the current folder. Is '.' in Windows
+	sep = str(sep) #The character used by the current os to denote the demotion to another folder level. Is '/' in Windows
 	from configobj import ConfigObj
 	charfolder = advfolder+"Characters"+sep
 	charfile = charfolder+charname+".azc"
@@ -23,58 +22,43 @@ def createchar( advfolder ) :
 	main = ConfigObj(mainfile, unrepr=True)
 	character['Basics'] = {}
 	character['Basics']['charname'] = charname
-	if main['Categories']['totalcategories'] != 0 :
-		totalcats = main['Categories']['totalcategories']
-		character['Categories'] = {}
-		remcats = int(totalcats)
-		while remcats != 0 :
-			print "Please select your character's "+main['Categories'][str(remcats)]['catname']+" from the options below:"
-			totalvals = main['Categories'][str(remcats)]['catvalues']
-			remvals = int(totalvals)
-			count = 0
-			while remvals != 0 :
-				count = count + 1
-				print str(count)+") "+main['Categories'][str(remcats)][str(remvals)]['valname']
-				remvals = remvals-1
-			valchoice = "no"
-			while (valchoice.isdigit()) == False or (int(valchoice) > totalvals) or (valchoice == "0"):
-				valchoice = raw_input(">")
-				if valchoice.isdigit() == False :
-					print "Please enter a number that coresponds to a listed option"
-				elif (int(valchoice) > totalvals) or (valchoice == "0") :
-					print "Number given is not in range of options"
-			character['Categories'][str(remcats)] = {}
-			character['Categories'][str(remcats)]['catname'] = main['Categories'][str(remcats)]['catname']
-			character['Categories'][str(remcats)]['valcode'] = ((totalvals) + 1) - int(valchoice)
-			character['Categories'][str(remcats)]['valname'] = main['Categories'][str(remcats)][str(((totalvals) + 1) - int(valchoice))]['valname']
-			remcats = remcats-1
-		remcats = int(totalcats)
+	catlist = main['Categories'].keys() #.agez files are read like dictionaries. This line grabs the keys (in this case the subsections that represent categories) from the chosen section
+	character['Categories'] = {}
+	for catno in catlist : #Do code below for every category in list
+		print "Please select your character's %s from the options below:" % (main['Categories'][catno]['catname'])
+		totalvals = len(main['Categories'][catno].keys())-1 #Unfortunately I cannot use the same technique for category values as 'catname' is a key. Instead the keys are counted and one is taken from them and I use a while loop
+		valno = 0
+		while valno < totalvals :
+			valno += 1 #Increments valno by 1
+			svalno = str(valno)
+			print svalno+") "+main['Categories'][catno][svalno]['valname']
+		valchoice = raw_input(">")
+		while (valchoice.isdigit()) == False or (int(valchoice) > totalvals) or (valchoice == "0"):
+			if valchoice.isdigit() == False :
+				print "Please enter a number that coresponds to a listed option"
+			elif (int(valchoice) > totalvals) or (valchoice == "0") :
+				print "Number given is not in range of options"
+			valchoice = raw_input(">")
+		character['Categories'][catno] = {}
+		character['Categories'][catno]['catname'] = main['Categories'][catno]['catname']
+		character['Categories'][catno]['valcode'] = int(valchoice)
+		character['Categories'][catno]['valname'] = main['Categories'][catno][valchoice]['valname']
 	csetup = main['Character Setup']
 	character['Basics']['scene'] = str(csetup['initialscene'])
 	if csetup['technique'] == 1 : #Will eventually add alternative character creation techniques
 		#Setting vitals
 		character['Vitals'] = {}
 		character['Vitals']['Initial Values'] = {}
-		charvits = list(csetup['Vitals'].keys())
-		charvitstot = len(charvits)
-		charvitsrem = len(charvits)
-		charvits.reverse()
-		while charvitsrem != 0 :
-			charvitno = charvits.pop()
-			charvitsrem = len(charvits)
+		charvits = csetup['Vitals'].keys()
+		for charvitno in charvits :
 			character['Vitals'][charvitno] = csetup['Vitals'][charvitno]['val']
 			character['Vitals']['Initial Values'][charvitno] = character['Vitals'][charvitno]
 		#Setting Attributes
 		from random import randint
 		character['Attributes'] = {}
 		character['Attributes']['Initial Values'] = {}
-		charatts = list(csetup['Attributes'].keys())
-		charattstot = len(charatts)
-		charattsrem = len(charatts)
-		charatts.reverse()
-		while charattsrem != 0 :
-			charattno = charatts.pop()
-			charattsrem = len(charatts)
+		charatts = csetup['Attributes'].keys()
+		for charattno in charatts :
 			character['Attributes'][charattno] = randint(int(csetup['Attributes'][charattno]['minval']), int(csetup['Attributes'][charattno]['maxval']))
 			character['Attributes']['Initial Values'][charattno] = character['Attributes'][charattno]
 		#Setting Inventory & Equipment
@@ -82,48 +66,55 @@ def createchar( advfolder ) :
 		character['Items']['inventory'] = csetup['Inventory']['val']
 		character['Items']['Equipment'] = {}
 		charequips = csetup['Equipment'].keys()
-		charequipstot = len(charequips)
-		charequipsrem = len(charequips)
-		charequips.reverse()
-		while charequipsrem != 0 :
-			charequipsno = charequips[(charequipsrem-1)]
-			charequipsrem -= 1
-			character['Items']['Equipment'][charequipsno] = csetup['Equipment'][charequipsno]['id']
+		for charequipno in charequips :
+			character['Items']['Equipment'][charequipno] = csetup['Equipment'][charequipno]['id']
 		#Setting currencies
 		character['Currency'] = {}
-		currencytot = main['Currencies']['total']
-		currencyno = 0
-		while currencyno != currencytot :
-			currencyno += 1
-			scurrencyno = str(currencyno)
-			if scurrencyno in csetup['Currencies'].keys() :
-				character['Currency'][scurrencyno] = csetup['Currencies'][scurrencyno]['val']
+		currencytypes = main['Currencies'].keys()
+		for currencyno in currencytypes :
+			if currencyno in csetup['Currencies'].keys() :
+				character['Currency'][currencyno] = csetup['Currencies'][currencyno]['val']
 			else :
-				character['Currency'][scurrencyno] = 0
+				character['Currency'][currencyno] = 0
 		character['Scene States'] = {}
 	elif csetup['technique'] == 2 :
 		pass
 	character.write()
 	print "New character created"
 	print ""		
-if __name__ == '__main__':
+if __name__ == '__main__': #If agccreator is run directly code below is run to determine 'advfolder' which is then fed into the above function
 	try:
 		import configobj #I'm using configobj instead of the built-in Configeditor as it allows for nested sections and list values
 	except ImportError, e:
-		raw_input("ConfigObj module is required. Please install and try again")
-		exit(0)
+		err = raw_input("ConfigObj module is required. Please install and try again")
+		if err != "Override" :
+			exit(0)
 	from configobj import *
-	import os
-	dot = str(os.curdir) #The character used by the current os to denote the current folder. Is '.' in Windows
-	sep = str(os.sep) #The character used by the current os to denote the demotion to another folder level. Is '/' in Windows
-	mainfile = dot+sep+"main.agez"
+	from os import curdir, sep, access, listdir, R_OK, name, system
+	from time import sleep
+	from subprocess import Popen
+	from itertools import repeat
+	dot = str(curdir) #The character used by the current os to denote the current folder. Is '.' in Windows
+	sep = str(sep) #The character used by the current os to denote the demotion to another folder level. Is '/' in Windows
+	if name == 'posix': #If OS is linux-based
+		def clr():
+			system('clear') #'Clr' will now execute the windows command 'clear' which clears the terminal in Linux
+	elif (name == 'nt') or (name == 'ce') or (name == 'dos') : #If OS is Windows
+		def clr():
+		   system('cls') #'Clr' will now execute the windows command 'cls' which clears the terminal in Windows
+	else :
+		def clr():
+			print "\n" * 10
+	#Hopefully the use of these will help make the engine cross-platform
+	advfolder = dot+sep
+	mainfile = advfolder+"main.agez"
 	advsfolder = "%s%sAdventures%s" %(dot,sep,sep)
 	print "Welcome to the Agenzig Character Creator"
-	if (os.access(advsfolder, os.R_OK)) and (str(os.listdir(advsfolder)) != "[]") :
-		done = 0
+	if (access(advsfolder, R_OK)) and (str(listdir(advsfolder)) != "[]") :
+		advchosen = 0
 		repeat = 0
-		while done == 0 :
-			advs = os.listdir(advsfolder)
+		while advchosen == 0 :
+			advs = listdir(advsfolder)
 			advs.reverse()
 			advsno = len(advs)
 			if repeat == 0 :
@@ -142,11 +133,11 @@ if __name__ == '__main__':
 			elif choice.isdigit() == 1 : #This section basically does the reverse of the above one to determine what adventure the inputted number refers to
 				sel = int(choice)
 				if sel <= opt :
-					advs2 = os.listdir(advsfolder)
+					advs2 = listdir(advsfolder)
 					advfolder = advsfolder+str(advs2.pop((sel-1)))+sep
 					mainfile = advfolder+sep+"main.agez"
-					if os.access(mainfile, os.R_OK) :
-						done = 1
+					if access(mainfile, R_OK) :
+						advchosen = 1
 					else :
 						print "Selected adventure folder does not contain a main file"
 				else:
@@ -156,15 +147,17 @@ if __name__ == '__main__':
 		main = ConfigObj(mainfile, unrepr=True)
 		advname = main['Details']['title']
 		charfolder = dot+sep+advfolder+sep+"Characters"+sep
-		print advname+" succesfully loaded\n"		
-	elif os.access(mainfile, os.R_OK) :
+		print advname+" succesfully loaded\n"
+		sleep(2)
+		clr()
+	elif access(mainfile, R_OK) :
 		main = ConfigObj(mainfile, unrepr=True)
 		advname = main['Details']['title']
 		charfolder = dot+sep+"Characters"+sep
 		advfolder = dot+sep
 		print "You will be creating a character for the %s adventure\n" %(advname)
 	else :
-		if os.access(advsfolder, os.R_OK) :
+		if access(advsfolder, R_OK) :
 			print "The Adventures folder exists but contains no Adventure folders\n"
 		else :
 			print "No Adventures folder found in script directory\n"
