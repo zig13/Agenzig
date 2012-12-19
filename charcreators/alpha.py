abf556256c82b29d517844cfadc32ab92e4fc74d
Thomas Sturges-Allard <zig13@termisoc.org>
2012-12-19 17:05:38 +0000

    Broke character creation script into the setup part which is now "new_character.py" and the character creation function which is now "alpha.py" to be found in the sub-folder "charcreators".

---
+++ charcreators/alpha.py
@@ -0,0 +1,64 @@
+def createchar(path) :
+	charname = raw_input("Please enter a name for your character >" )
+	from configobj import ConfigObj
+	charfile = path.charfolder+charname+".azc"
+	character = ConfigObj(charfile, unrepr=True)
+	main = ConfigObj(path.main, unrepr=True)
+	character['Basics'] = {}
+	character['Basics']['charname'] = charname
+	catlist = main['Categories'].keys() #.agez files are read like dictionaries. This line grabs the keys (in this case the subsections that represent categories) from the chosen section
+	character['Categories'] = {}
+	for catno in catlist : #Do code below for every category in list
+		print "Please select your character's %s from the options below:" % (main['Categories'][catno]['catname'])
+		totalvals = len(main['Categories'][catno].keys())-1 #Unfortunately I cannot use the same technique for category values as 'catname' is a key. Instead the keys are counted and one is taken from them and I use a while loop
+		valno = 0
+		while valno < totalvals :
+			valno += 1 #Increments value by 1
+			svalno = str(valno)
+			print svalno+") "+main['Categories'][catno][svalno]['valname']
+		valchoice = raw_input(">")
+		while (valchoice.isdigit()) == False or (int(valchoice) > totalvals) or (valchoice == "0"):
+			if valchoice.isdigit() == False :
+				print "Please enter a number that coresponds to a listed option"
+			elif (int(valchoice) > totalvals) or (valchoice == "0") :
+				print "Number given is not in range of options"
+			valchoice = raw_input(">")
+		character['Categories'][catno] = {}
+		character['Categories'][catno]['catname'] = main['Categories'][catno]['catname']
+		character['Categories'][catno]['valcode'] = int(valchoice)
+		character['Categories'][catno]['valname'] = main['Categories'][catno][valchoice]['valname']
+	csetup = main['Character Setup']
+	character['Basics']['scene'] = str(csetup['initialscene'])
+	#Setting vitals
+	character['Vitals'] = {}
+	character['Vitals']['Initial Values'] = {}
+	charvits = csetup['Vitals'].keys()
+	for charvitno in charvits :
+		character['Vitals'][charvitno] = csetup['Vitals'][charvitno]['val']
+		character['Vitals']['Initial Values'][charvitno] = character['Vitals'][charvitno]
+	#Setting Attributes
+	from random import randint
+	character['Attributes'] = {}
+	character['Attributes']['Initial Values'] = {}
+	charatts = csetup['Attributes'].keys()
+	for charattno in charatts :
+		character['Attributes'][charattno] = randint(int(csetup['Attributes'][charattno]['minval']), int(csetup['Attributes'][charattno]['maxval']))
+		character['Attributes']['Initial Values'][charattno] = character['Attributes'][charattno]
+	#Setting Inventory & Equipment
+	character['Items'] = {}
+	character['Items']['inventory'] = csetup['Inventory']['val']
+	character['Items']['Equipment'] = {}
+	charequips = csetup['Equipment'].keys()
+	for charequipno in charequips :
+		character['Items']['Equipment'][charequipno] = csetup['Equipment'][charequipno]['id']
+	#Setting currencies
+	character['Currency'] = {}
+	currencytypes = main['Currencies'].keys()
+	for currencyno in currencytypes :
+		if currencyno in csetup['Currencies'].keys() :
+			character['Currency'][currencyno] = csetup['Currencies'][currencyno]['val']
+		else :
+			character['Currency'][currencyno] = 0
+	character['Scene States'] = {}
+	character.write()
+	return True	
---
+++ new_character.py
@@ -0,0 +1,67 @@
+#-------------------------------------------------------------------------------
+# Name:        Agenzig Character Creator
+# Purpose:     Character creator for a the text-based adventure game engine - Agenzig
+#
+# Author:      Thomas Sturges-Allard
+#
+# Created:     15/12/2012
+# Copyright:   (c) Thomas Sturges-Allard 2011
+# Licence:      Licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+#			http://creativecommons.org/licenses/by-nc-sa/3.0/
+#-------------------------------------------------------------------------------
+try:
+	from configobj import ConfigObj #I'm using configobj instead of the built-in Configeditor as it allows for nested sections and list values
+except ImportError, e:
+	raw_input("ConfigObj module is required. Please install and try again")
+	exit(0)
+
+from classes.classPath import Path
+path = Path()
+
+if path.validate() == False :
+	raw_input("No adventures are installed to create characters for.\nAdventures should be in individual folders inside a directory named 'Adventures' within this one.")
+	exit(0)
+
+from functions import Clr, yesno, choicelist
+while True :
+	success = False
+	if path.advstotal == 1 :
+		from classes.classAdventure import Adventure, validation_fail
+		adventure = Adventure(path.adventures[0])
+		try :
+			adventure.validate()
+		except validation_fail as e:
+			print e.reason, "from adventure folder.\n"
+			raw_input("Character creation cannot continue")
+			exit(0)
+	else :
+		from classes.classAdventure import Adventures, Adventure, validation_fail
+		adventures = Adventures(path)
+		print "Listing Adventures:"
+		while True :
+			print "Which adventure would you like to make a character for?"
+			choice = choicelist(path.adventures)
+			try :		
+				adventure = Adventure(choice[1])
+				adventure.validate()
+				break
+			except validation_fail as e:
+				print e.reason, "\nPlease choose a different adventure\n"
+	
+	adventure.details()
+	
+	exec "from charcreators."+adventure.charcreator+" import createchar"
+	success = bool(createchar(adventure.path))
+	Clr()
+	if success == True :
+		print "Character created successfully\n"
+	else :
+		print "Character creation failed\n"
+	print "Would you like to create another?"
+	answer = bool(yesno())
+	Clr()
+	if answer == False :
+		break
+	
+
+		
