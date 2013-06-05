2af1ef5029c3a1281d9ab48e3b69d74a8159a841
Thomas Sturges-Allard <zig13@termisoc.org>
2013-06-05 19:10:53 +0100

    Finalised the 14 "optional" data files available to Adventure makers to customise thier adventure and the validation system for them. They are abilities, actions, actions_encounter, attributes, categories, choices, classifications, currencies, encounters, equipment, equipment_slots, items, scenes and vitals

--- classes/classAdventure.py
+++ classes/classAdventure.py
@@ -4,21 +4,12 @@ class Adventure :
 		self.path = Path(foldername)
 	def validate(self) :
 		from os import access, R_OK
-		if not access(self.path.main, R_OK) : raise validation_fail("Main file missing")
-		if not access(self.path.scenes, R_OK) : raise validation_fail("Scenes file missing")
+		if not access(self.path.advfile['Main'], R_OK) : raise validation_fail("Main file missing")
 		from configobj import ConfigObj
-		self.file = ConfigObj(self.path.main, unrepr=True)
-		self.requirements = self.file['Details']['requirements']
-		self.path.reqfiles(self.requirements)
-		if self.requirements[0] == 1 and not access(self.path.abilities, R_OK) : raise validation_fail("abilities file missing")
-		if self.requirements[1] == 1 and not access(self.path.attributes, R_OK) : raise validation_fail("attributes file missing")
-		if self.requirements[2] == 1 and not access(self.path.categories, R_OK) : raise validation_fail("categories file missing")
-		if self.requirements[3] == 1 and not access(self.path.choices, R_OK) : raise validation_fail("choices file missing")
-		if self.requirements[4] == 1 and not access(self.path.confrontations, R_OK) : raise validation_fail("confrontations file missing")
-		if self.requirements[5] == 1 and not access(self.path.equipment, R_OK) : raise validation_fail("equipment file missing")
-		if self.requirements[6] == 1 and not access(self.path.equipslots, R_OK) : raise validation_fail("equipslots file missing")
-		if self.requirements[7] == 1 and not access(self.path.items, R_OK) : raise validation_fail("items file missing")
-		if self.requirements[8] == 1 and not access(self.path.vitals, R_OK) : raise validation_fail("vitals file missing")
+		self.file = ConfigObj(self.path.advfile['Main'], unrepr=True)
+		reqfiles = self.path.datafiles(self.file['Details']['required_data_files'])
+		for fileindex in reqfiles :
+			if not access(self.path.advfile[fileindex], R_OK) : raise validation_fail(self.path.filenames[fileindex]+" file missing")
 		
 	def details(self) :
 		self.title = self.file['Details']['title']
--- classes/classPath.py
+++ classes/classPath.py
@@ -1,4 +1,6 @@
 class Path :	
+	filenames = ['abilities', 'actions', 'actions_encounter', 'attributes', 'categories', 'choices', 'classifications', 'currencies', 'encounters', 'equipment', 'equipment_slots', 'items', 'scenes', 'vitals']
+	advfile = {}
 	def __init__(self, foldername=None) :
 		from os import pardir, curdir, sep, listdir
 		from os.path import abspath
@@ -15,25 +17,21 @@ class Path :
 				makedirs(self.charfolder)
 			self.characters = listdir(self.charfolder)
 			self.chartotal = len(self.characters)
-			self.main = self.folder+"Main.agez"
-			self.scenes = self.folder+"scenes.agez"
+			self.advfile['Main'] = self.folder+"Main.agez"
 		
 	def validate(self, requirements=None) :
 		from os import access, R_OK
 		if not access(self.advsfolder, R_OK) : raise validation_fail("Adventures folder missing")
 		if self.advstotal < 1 : raise validation_fail("No adventures installed")		
 		
-	def reqfiles(self, requirements) :
-		if requirements[0] == 1 : self.abilities = self.folder+"abilities.agez"
-		if requirements[1] == 1 : self.attributes = self.folder+"attributes.agez"
-		if requirements[2] == 1 : self.cabilities = self.folder+"cabilities.agez"
-		if requirements[3] == 1 : self.categories = self.folder+"categories.agez"
-		if requirements[4] == 1 : self.choices = self.folder+"choices.agez"
-		if requirements[5] == 1 : self.encounters = self.folder+"encounters.agez"
-		if requirements[6] == 1 : self.equipment = self.folder+"equipment.agez"
-		if requirements[7] == 1 : self.equipslots = self.folder+"equipslots.agez"
-		if requirements[8] == 1 : self.items = self.folder+"items.agez"
-		if requirements[9] == 1 : self.vitals = self.folder+"vitals.agez"
+	def datafiles(self, boolfiles) :
+		reqfiles = []
+		for index, reqbool in enumerate(reqfiles) :
+			if reqbool == True : 
+				self.advfile[self.filenames[index]] = (self.folder+self.filenames[index]+".agez")
+				self.advfile[index] = (self.folder+self.filenames[index]+".agez")
+				reqfiles.append(index)
+		return reqfiles
 	
 	def setchar(self, charname) :
 		self.charfile = self.charfolder+charname
